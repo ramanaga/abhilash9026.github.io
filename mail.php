ad7cf805e3dc460e733a5c9213eca1b93ae3f58d
Abhilash <abhilash9026@outlook.com>
2018-10-12 00:36:32 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,12 @@
+<?php 
+if ($_POST["email"]<>'') { 
+    $ToEmail = 'abhilashm9026@gmail.com'; 
+    $EmailSubject = 'Site contact form'; 
+    $mailheader = "From: ".$_POST["email"]."\r\n"; 
+    $mailheader .= "Reply-To: ".$_POST["email"]."\r\n"; 
+    $mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
+    $MESSAGE_BODY = "Name: ".$_POST["name"].""; 
+    $MESSAGE_BODY .= "Email: ".$_POST["email"].""; 
+    $MESSAGE_BODY .= "Message: ".nl2br($_POST["message"]).""; 
+    mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Failure"); 
+?>
